507adcb31eac310c9ce513afc49bb0eea0b027f7
RedBoard <ott.thomas68@gmail.com>
2023-08-29 02:17:03 +0200

    (+) implement random background image

--- chrome_extension/src/App.js
+++ chrome_extension/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Paper } from '@mui/material';
-import backgroundImage from './assets/background.jpg';
 import TimeDisplay from './composants/TimeDisplay';
 import WeatherWidget from './composants/WeatherWidget';
 import CryptoWidget from './composants/CryptoWidget';
@@ -8,8 +7,19 @@ import CryptoWidget from './composants/CryptoWidget';
 function App() {
     const [latitude, setLatitude] = useState(null);
     const [longitude, setLongitude] = useState(null);
+    const [backgroundImage, setBackgroundImage] = useState(null);
+
+    const fetchRandomImage = () => {
+        const width = 1920;
+        const height = 1080;
+        const imageUrl = `https://picsum.photos/${width}/${height}?random`;
+        setBackgroundImage(imageUrl);
+    };
+
 
     useEffect(() => {
+        fetchRandomImage();
+
         if ('geolocation' in navigator) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
@@ -27,13 +37,20 @@ function App() {
 
     const paperStyles = {
         minHeight: '100vh',
-        backgroundImage: `url(${backgroundImage})`,
+        backgroundImage: `
+            linear-gradient(
+                rgba(0, 0, 0, 0.3),
+                rgba(0, 0, 0, 0.3)
+            ),
+            url(${backgroundImage})
+        `,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        borderRadius: 0,
     };
 
     return (
